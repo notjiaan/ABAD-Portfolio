dab1f38f5417ebec621565f024d0f49939bf456a
Jiaan Abad <jiaanpatrick.abad@gmail.com>
2022-03-18 16:34:02 +0800

    updated files

--- Node-Assignment/server.js
+++ Node-Assignment/server.js
@@ -10,50 +10,31 @@ const server = http.createServer((req, res) => {
   let path = "./web-page/";
 
   //append to log file the url
-  const appendLogs = () => {
-    fs.appendFile(
-      "./logs/log.txt",
-      `localhost:${PORT}` + req.url + "\n",
-      () => {
-        console.log("url added to log file\n");
-      }
-    );
 
-    //console log current url and status code
-    console.log(`localhost:${PORT}` + req.url);
-    console.log(res.statusCode);
-  };
+  fs.appendFile(
+    "./logs/logs.txt",
+    "The user visited: " + req.url + "\n" + "<br />",
+    () => {
+      console.log("url added to log file\n");
+    }
+  );
 
-  const showHistoryLogs = () => {
-    fs.readFile("./logs/logs.txt", (err, txtString) => {
-      if (err) {
-        throw err;
-      } else {
-        console.log(txtString.toString());
-        // replace new line (\n) to <br/>
-        res.write("<div>" + txtString.toString().replace(/\n/g, "<br />"));
-
-        console.log("history displayed\n");
-      }
-    });
-  };
   //switch case router for views
   switch (req.url) {
     case "/":
       path += "index.html";
       res.statusCode = 200;
-      appendLogs();
       break;
 
     case "/about":
       path += "about.html";
       res.statusCode = 200;
-      appendLogs();
       break;
 
     case "/history":
-      path += "history.html";
-      showHistoryLogs();
+      res.setHeader("Content-type", "text/html");
+      path += "../logs/logs.txt";
+      res.statusCode = 200;
       break;
 
     case "/secret":
